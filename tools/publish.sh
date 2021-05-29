e1799235904bc8e3e2ca7feb308aeece9814b5aa
Aleksej Komarov <stylemistake@gmail.com>
2021-05-30 01:32:25 +0300

    Add a release tool

---
+++ tools/publish.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+cd "$(dirname "${0}")/.."
+set -e
+
+from_git=0
+
+for arg in "${@}"; do
+  if [[ ${arg} == "--from-git" ]]; then
+    from_git=1
+    continue
+  fi
+done
+
+yarn install
+
+echo "=> Verifying login"
+if ! yarn npm whoami --publish; then
+  exit 1
+fi
+
+if [[ ${from_git} == "0" ]]; then
+  echo "=> Starting versioning tool"
+  yarn release-tool version
+fi
+
+echo "=> Starting publishing tool"
+yarn release-tool publish
+
+echo "=> Pushing to git"
+git push
+git push --tags
+
+echo "=> Done"
